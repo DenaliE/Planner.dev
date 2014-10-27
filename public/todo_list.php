e0c8c1dce6e78ae371f087784af8d9bbe691bd86
DenaliE <denali.endicott@grok-interactive.com>
2014-10-27 18:57:47 -0500

    Browser version 1

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,27 +1,92 @@
-<?php 
-var_dump($_GET);
-var_dump($_POST);
+<?php
+ 
+// Define a function which will open your default filename, and return an array of items.
+
+function open ($filename = 'list.txt'){
+
+	if (filesize($filename) > 0){
+		$filesize = filesize($filename);
+	}
+
+	else {
+		$filesize = 100;
+	}
 
 
+	$handle = fopen($filename, 'r');
+	$contents = trim(fread($handle, $filesize));
+	$contentArray = explode("\n", $contents);
+
+	fclose($handle);
+
+	return $contentArray;
+
+}
+
+
+ 
+/* This function accepts a filename, and returns an array of list items. */
+ 
+// Define a function which will save your list to file.
+
+function save($array, $filename = 'list.txt'){
+
+	$handle = fopen($filename, 'w');
+	$string = implode("\n", $array);
+	fwrite($handle, $string);
+	fclose($handle);
+}
+ 
+/* This function accepts an array, saves it to file, and returns nothing. */
+ 
+// Initialize your array by calling your function to open file.
+ 
+$items = open();
+
+
+// Check for GET Requests
+    // If there is a get request; remove the appropriate item.
+if (isset($_GET['id'])){
+	$id = $_GET['id'];
+	unset($items[$id]);
+	save($items);
+}
+ 
+// Check for POST Requests
+    // If there is a post request; add the items.
+//$_POST['nameOfInput']
+if (isset($_POST['newitem'])) {
+	//Assign newitem from the form the $itemToAdd.
+	$itemToAdd = $_POST['newitem'];
+	//Array push that new item onto the existing list.
+	//alternate way to do array push
+	$items[] = $itemToAdd;
+	// Save the whole list to file.
+	save($items);
+}
+ 
 ?>
-
-<!DOCTYPE html>
+ 
 <html>
 <head>
-	<title>TODO List</title>
-	<link rel="stylesheet" type="text/css" href="/css/style.css" href="http://fonts.googleapis.com/css?family=Droid+Sans">
+    <title>TODO App</title>
 </head>
 <body>
-<h1 id="a">TODO List</h1>
 <ol>
-	<li>Be happy</li>
-	<li>Be calm</li>
-	<li>Tame cat</li>
+<!-- Echo Out the List Items -->
+<?php foreach ($items as $key => $item) {
+	echo "<li>"."<a href=\"?id=$key\">X</a>". $item."</li>";
+	
+}?>
 </ol>
-<h2>Add item</h2>
-<form method="POST" action="/todo_list.php">
-<label><input type="text" name="input" id="todo"></input></label>	
-<button type="submit">Add item</button>
+ 
+<!-- Create a Form to Accept New Items -->
+
+<form method="POST" name='add-form' action="todo_list.php">
+	<label for='newitem'>Add a new item: </label>
+	<input name='newitem' id='newitem' type='text'>
+	<button>Add Item</button>
 </form>
+ 
 </body>
 </html>
