92977976db5fe0eeed9d27be9edc0b10898aed77
DenaliE <denali.endicott@grok-interactive.com>
2014-10-28 11:04:16 -0500

    Added ability to upload file

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,5 +1,26 @@
 <?php
  
+ // Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
+    // Set the destination directory for uploads
+    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
+
+    // Grab the filename from the uploaded file by using basename
+    $filename = basename($_FILES['file1']['name']);
+
+    // Create the saved filename using the file's original name and our upload directory
+    $savedFilename = $uploadDir . $filename;
+
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+}
+
+// Check if we saved a file
+if (isset($savedFilename)) {
+    // If we did, show a link to the uploaded file
+    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+}
+
 // Define a function which will open your default filename, and return an array of items.
 
 function open($filename = 'list.txt'){
@@ -71,6 +92,17 @@ if (isset($_POST['newitem'])) {
     <title>TODO App</title>
 </head>
 <body>
+<h1>Upload File</h1>
+
+    <form method="POST" enctype="multipart/form-data" action="/todo_list.php">
+        <p>
+            <label for="file1">File to upload: </label>
+            <input type="file" id="file1" name="file1">
+        </p>
+        <p>
+            <input type="submit" value="Upload">
+        </p>
+    </form>
 <ol>
 <!-- Echo Out the List Items -->
 <?php foreach ($items as $key => $item) {
