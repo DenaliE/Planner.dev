9d89efa3d61ad3d96da1abdd86c94fd7d77e5bda
DenaliE <denali.endicott@grok-interactive.com>
2014-11-01 13:23:38 -0500

    Refactoring. Still working on upload.

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,117 +1,130 @@
 <?php
  
-// Initialize your array by calling your function to open file.
+class Todo {
+	// Define a function which will open your default filename, and return an array of items.
+
+	public $filename = 'list.txt';
+
+	public $items = [];
+
+	public function open(){
+
+		$filesize = filesize($this->filename);
+
+		$contents = [];
+
+		if (file_exists($this->filename) && $filesize > 0) {
+			$handle = fopen($this->filename, 'r');
+			$contents = trim(fread($handle, $filesize));
+			$contents = explode("\n", $contents);
+
+			fclose($handle);	
+		}
+
+		return $contents;
+
+	}
+
+
+	//sanitizes input
+	//why is this not working?
+	public function sanitize(){
+		foreach ($this->items as $key => $value) {
+			//echo $value; dirty
+			$this->items[$key] = htmlspecialchars(strip_tags($value));//Overwrite the value
+			//echo $value; clean
+		}
+		
+		return $this->items;
+		
+	}
  
-$items = open();
+// /* This function accepts a filename, and returns an array of list items. */
+ 
+// // Define a function which will save your list to file.
 
-
- // Verify there were uploaded files and no errors
-if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
-    // Set the destination directory for uploads
-    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
-
-    // Grab the filename from the uploaded file by using basename
-    $filename = basename($_FILES['file1']['name']);
-
-    // Create the saved filename using the file's original name and our upload directory
-    $savedFilename = $uploadDir . $filename;
-
-    // Move the file from the temp location to our uploads directory
-    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
-
-    //check for test file
-    if ($_FILES['file1']['type'] == 'text/plain'){
-    $newItems = open($savedFilename);
-	$items = array_merge($items, $newItems);
-	save($items);
- 	}
-}
-
-// Check if we saved a file
-if (isset($savedFilename)) {
-    // If we did, show a link to the uploaded file
-    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
-}
-
-
-
-
-
-// Define a function which will open your default filename, and return an array of items.
-
-function open($filename = 'list.txt'){
-
-	if (file_exists($filename) && (filesize($filename)) > 0){
-		$filesize = filesize($filename);
-		$handle = fopen($filename, 'r');
-		$contents = trim(fread($handle, $filesize));
-		$contentArray = explode("\n", $contents);
-
+	public function save(){
+		$cleanArray = $this->sanitize();
+		$handle = fopen($this->filename, 'w');
+		$string = implode("\n", $cleanArray);
+		fwrite($handle, $string);
 		fclose($handle);
 	}
+ 
+// /* This function accepts an array, saves it to file, and returns nothing. */
+ 
+	public function upload(){
+		// Verify there were uploaded files and no errors
+		if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
+		    // Set the destination directory for uploads
+		    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
 
-	else {
-		$contentArray = [];
+		    // Grab the filename from the uploaded file by using basename
+		    $file = basename($_FILES['file1']['name']);
+		  
+
+		    // Create the saved filename using the file's original name and our upload directory
+		    $savedFilename = $uploadDir . $filename;
+		  
+
+		    // Move the file from the temp location to our uploads directory
+		    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+
+		    //check for test file
+		    if ($_FILES['file1']['type'] == 'text/plain'){
+		    $newItems = open($savedFilename);
+			$contentArray = array_merge($contentArray, $newItems);
+			save($contentArray);
+		 	}
+		}
+
+
+	return $savedFilename;
+
+
 	}
 
-	return $contentArray;
-
 }
 
-//sanitizes input
-function sanitize($array){
-	foreach ($array as $key => $value) {
-		//echo $value; dirty
-		$array[$key] = htmlspecialchars(strip_tags($value));//Overwrite the value
-		//echo $value; clean
+// // Initialize your array by calling your function to open file.
+ 
+$ListObj = new Todo();
+$ListObj->items = $ListObj->open();
+
+// 	// Check if we saved a file
+	if (isset($savedFilename)) {
+	    // If we did, show a link to the uploaded file
+	    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
 	}
-	
-	return $array;
-	
-}
- 
-/* This function accepts a filename, and returns an array of list items. */
- 
-// Define a function which will save your list to file.
-
-function save($array, $filename = 'list.txt'){
-	//var_dump($array);
-	//sanitize($array);
-	//var_dump($array);
-	$array = sanitize($array);
-	$handle = fopen($filename, 'w');
-	$string = implode("\n", $array);
-	fwrite($handle, $string);
-	fclose($handle);
-}
- 
-/* This function accepts an array, saves it to file, and returns nothing. */
- 
 
 
-// Check for GET Requests
-    // If there is a get request; remove the appropriate item.
-if (isset($_GET['id'])){
-	$id = $_GET['id'];
-	$items = array_values($items);
-	unset($items[$id]);
+// 	// Check for GET Requests
+	    // If there is a get request; remove the appropriate item.
+	if (isset($_GET['id'])){
+		$id = $_GET['id'];
+		$ListObj->items = array_values($ListObj->items);
+		unset($ListObj->items[$id]);
+		$ListObj->save();
+	}
+	 
+	// Check for POST Requests
+	    // If there is a post request; add the items.
+	//$_POST['nameOfInput']
+	if (isset($_POST['newitem'])) {
+		//Assign newitem from the form the $itemToAdd.
+		$itemToAdd = $_POST['newitem'];
+		
 
-	save($items);
-}
- 
-// Check for POST Requests
-    // If there is a post request; add the items.
-//$_POST['nameOfInput']
-if (isset($_POST['newitem'])) {
-	//Assign newitem from the form the $itemToAdd.
-	$itemToAdd = $_POST['newitem'];
-	//Array push that new item onto the existing list.
-	//alternate way to do array push
-	$items[] = $itemToAdd;
-	// Save the whole list to file.
-	save($items);
-}
+		//sanitze
+		// $itemToAdd = $ListObj->sanitize($itemToAdd);
+		
+		//Array push that new item onto the existing list.
+		//alternate way to do array push
+		$ListObj->items[] = $itemToAdd;
 
+		// Save the whole list to file.
+		$ListObj->save();
+	}
 
 ?>
  
@@ -135,7 +148,7 @@ if (isset($_POST['newitem'])) {
     </form>
 <ol>
 
-<? foreach ($items as $key => $item): ?>
+<? foreach ($ListObj->items as $key => $item): ?>
 	<li>
 		<a href="?id=<?=$key?>">X</a>
 		<?= htmlspecialchars(strip_tags($item)); ?>
