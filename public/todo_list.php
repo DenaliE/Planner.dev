98d84e80235f9fb5f024335b7b25ab288a7ccaf4
Denali Endicott <denali@Denalis-MacBook-Pro.local>
2014-11-13 15:37:16 -0600

    Uses include file

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,112 +1,42 @@
 <?php
- 
-class Todo {
-	// Define a function which will open your default filename, and return an array of items.
 
-	public $filename = '';
+require_once('../inc/filestore.php');
+
+class Todo extends Filestore
+{
+	// Define a function which will open your default filename, and return an array of items.
 
 	public $items = [];
 
-	function __construct($filename = 'list.txt')
-    {
-       $this->filename = $filename; 
-    }
 
-
-
-	public function open(){
-
-		$filesize = filesize($this->filename);
-
-		$contents = [];
-
-		if (file_exists($this->filename) && $filesize > 0) {
-			$handle = fopen($this->filename, 'r');
-			$contents = trim(fread($handle, $filesize));
-			$contents = explode("\n", $contents);
-
-			fclose($handle);	
-		}
-
-		return $contents;
-
-	}
 	//sanitizes input
 	public function sanitize_array(){
 		foreach ($this->items as $key => $value) {
 			//this echos the contents of the whole object. Why?
-			
+
 			$this->items[$key] = htmlspecialchars(strip_tags($value));//Overwrite the value
 			//var_dump($value); //clean
 		}
-		
+
 		return $this->items;
-		
+
 	}
 
 	public function sanitize_string($dirty_string){
-			
+
 		$clean_string = htmlspecialchars(strip_tags($dirty_string));//Overwrite the value
-		
+
 		return $clean_string;
-		
+
 	}
- 
-// /* This function accepts a filename, and returns an array of list items. */
- 
-// // Define a function which will save your list to file.
 
-	public function save(){
-		$cleanArray = $this->sanitize_array();
-		$handle = fopen($this->filename, 'w');
-		$string = implode("\n", $cleanArray);
-		fwrite($handle, $string);
-		fclose($handle);
-	}
- 
-// /* This function accepts an array, saves it to file, and returns nothing. */
- 
-	// public function upload(){
-	// 	// Verify there were uploaded files and no errors
-	// 	if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
-	// 		var_dump($_FILES);
-	// 	    // Set the destination directory for uploads
-	// 	    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
-
-	// 	    // Grab the filename from the uploaded file by using basename
-	// 	    $file = basename($_FILES['file1']['name']);
-	// 	    var_dump($file);
-		  
-
-	// 	    // Create the saved filename using the file's original name and our upload directory
-	// 	    $savedFilename = $uploadDir . $filename;
-		  
-
-	// 	    // Move the file from the temp location to our uploads directory
-	// 	    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
-
-
-	// 	    //check for test file
-	// 	    if ($_FILES['file1']['type'] == 'text/plain'){
-	// 	    $newItems = open($savedFilename);
-	// 		$contentArray = array_merge($contentArray, $newItems);
-	// 		upload($contentArray);
-	// 		save($contentArray);
-	// 	 	}
-	// 	}
-
-
-	// return $contentArray;
-
-
-	// }
 
 }
 
 // // Initialize your array by calling your function to open file.
- 
+
 $ListObj = new Todo();
-$ListObj->items = $ListObj->open();
+$ListObj->items = $ListObj->readLines();
 //var_dump($ListObj->items);
 
 // 	// Check if we saved a file
@@ -122,9 +52,9 @@ $ListObj->items = $ListObj->open();
 		$id = $_GET['id'];
 		$ListObj->items = array_values($ListObj->items);
 		unset($ListObj->items[$id]);
-		$ListObj->save();
+		$ListObj->writeLines();
 	}
-	 
+
 	// Check for POST Requests
 	    // If there is a post request; add the items.
 	//$_POST['nameOfInput']
@@ -132,55 +62,55 @@ $ListObj->items = $ListObj->open();
 		//Assign newitem from the form the $itemToAdd.
 		$itemToAdd = $_POST['newitem'];
 		var_dump($itemToAdd);
-		
+
 
 		$itemToAdd = $ListObj->sanitize_string($itemToAdd);
-		
+
 		//Array push that new item onto the existing list.
 		//alternate way to do array push
 		$ListObj->items[] = $itemToAdd;
 
 		// Save the whole list to file.
-		$ListObj->save();
+		$ListObj->writeLines();
 	}
 
 	//to upload files, there needs to be a file with content
 	// Verify there were uploaded files and no errors
 	if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
-		
+
 	    // Set the destination directory for uploads
 	    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
 
 	    // Grab the filename from the uploaded file by using basename
 	    $filename = basename($_FILES['file1']['name']);
-	    
-	  
+
+
 
 	    // Create the saved filename using the file's original name and our upload directory
 	    $savedFilename = $uploadDir . $filename;
-	  
+
 
 	    // Move the file from the temp location to our uploads directory
 	    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
 
 	    //make new object. pass it the user entered $filename
 	    $NewListObj = new Todo($filename);
-	    
+
 	    //open the object to access the file contents. When it opens, it looks for the file's content (items)
 	    //Save the file contents to the object's items.
-	    $NewListObj->items = $NewListObj->open();
-	    
+	    $NewListObj->items = $NewListObj->readLines();
+
 
 	    //merge the file's items (contents) to the original objects items
 	    $ListObj->items = array_merge($ListObj->items, $NewListObj->items);
-	    
+
 	    //save it
-	    $ListObj->save();
-	    
+	    $ListObj->writeLines();
+
 	 }
 
 ?>
- 
+
 <html>
 <head>
     <title>TODO App</title>
@@ -209,7 +139,7 @@ $ListObj->items = $ListObj->open();
 <? endforeach ?>
 
 </ol>
- 
+
 <!-- Create a Form to Accept New Items -->
 
 <form method="POST" name='add-form' action="todo_list.php">
@@ -217,6 +147,6 @@ $ListObj->items = $ListObj->open();
 	<input name='newitem' id='newitem' type='text'>
 	<button>Add Item</button>
 </form>
- 
+
 </body>
 </html>
