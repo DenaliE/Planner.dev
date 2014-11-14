f0213879c22f874b780e877395fd86c1b3258d58
denalie <denali.endicott@gmail.com>
2014-11-14 16:04:54 -0600

    It works. Uses required file.

--- public/address_book.php
+++ public/address_book.php
@@ -1,133 +1,95 @@
-<?php 
+<?php
 
+require_once('../inc/filestore.php');
+define('FILE', 'address_book.csv');
 
-
-class addressBook {
+class AddressBook extends Filestore
+{
 
 	public $filename = '';
 	public $contents = [];
-
-	function __construct($filename = 'address_book.csv')
-    {
-       $this->filename = $filename; 
-    }
-	
-    public function open_csv() {
-    	
-    	$filesize = filesize($this->filename);
-
-    	if (file_exists($this->filename)){
-    			$handle = fopen($this->filename, 'r');
-
-    			while(!feof($handle)) {
-    			    $row = fgetcsv($handle);
-
-    			    if (!empty($row)) {
-    			        $this->contents[] = $row;
-    			    }
-    			}
-
-    			fclose($handle);
-    			return $this->contents;
-    		}
-
-    		else {
-    			touch($this->filename);
-    		}
-
-    	}
 
     	public function sanitize_array($array){
 			foreach ($array as $value) {
 
 					$clean_array[] = htmlspecialchars(strip_tags($value));//Overwrite the value
-				
+
 			}
-		
+
 
 			return $clean_array;
-		
-		}
-    	
-		public function save_csv(){
-		
-		//write to csv file
-		$handle = fopen($this->filename, 'a+');
-		foreach ($this->contents as $row) {
-		    fputcsv($handle, $row);
-		}// foreach
-
-		fclose($handle);
-
 
 		}
-   
+
+
 }//closes class
 
 
 // Display error if each is not filled out.
 
-$addressBook = new addressBook();
+$addressBook = new AddressBook(FILE);
 
-$addressBook->contents = $addressBook->open_csv();
+$addressBook->contents = $addressBook->readCSV();
 
 //check input
 if (!empty($_POST)) {
   if (empty($_POST['name']) || empty($_POST['phone']) || empty($_POST['address']) || empty($_POST['city']) || empty($_POST['state']) || empty($_POST['zip'])) {
 
-	$error = "Please enter all fields.";
-	//var_dump($error);
-	
+	echo $error = "Please enter all fields.";
+
 	} // if validation
 
 	else {
-		
+
 		if (isset($_POST['name'])){
 			$newEntry['name'] = $_POST['name'];
-			
+
 		}
 
 		if (isset($_POST['phone'])){
 			$newEntry['phone'] = $_POST['phone'];
-			
+
 		}
 
 		if (isset($_POST['address'])){
 			$newEntry['address'] = $_POST['address'];
-			
+
 		}
 
 		if (isset($_POST['city'])){
 			$newEntry['city'] = $_POST['city'];
-			
+
 		}
 
 		if (isset($_POST['state'])){
 			$newEntry['state'] = $_POST['state'];
-			
+
 		}
 		if (isset($_POST['zip'])){
 			$newEntry['zip'] = $_POST['zip'];
-			
+
 		}//else
 
-		
+
 
 		$cleanArray = $addressBook->sanitize_array($newEntry);
 		$addressBook->contents[] = $cleanArray;
-		$addressBook->save_csv($cleanArray);
+		$addressBook->writeCSV($addressBook->contents);
+
+
 
 //redirect to keep browser from offering to resubmit form
 //add output buffer
 //header("Location: http://planner.dev/address_book.php");
 
-	} // elseif 
+	} // elseif
 }// if post not empty
 
 if (isset($_GET['id'])){
 	$id = $_GET['id'];
-	unset($addressBook->contents[$id]);
-	$addressBook->save_csv();
+    unset($addressBook->contents[$id]);
+    //this needs a parameter, but I do I want to rewrite my whole array?
+    $addressBook->writeCSV($addressBook->contents);
 }
 
 ?>
@@ -152,30 +114,30 @@ if (isset($_GET['id'])){
 	</tr>
 		<? if (!is_null($addressBook->contents)) { ?>
 			<?  foreach ($addressBook->contents as $key => $address): ?>
-			<tr>	
+			<tr>
 					<?foreach ($address as $value): ?>
-			
+
 						<!-- insert each in table row -->
 						<td>
 							<?=$value?>
-						</td>	
-					<? endforeach; ?> 
-				<td> 
+						</td>
+					<? endforeach; ?>
+				<td>
 					<a href="?id=<?=$key?>">  x  </a><!-- targets key of each line, second level array-->
-				</td> 
+				</td>
 			</tr>
-		<? endforeach; ?> 
-		<? }; ?>							
+		<? endforeach; ?>
+		<? }; ?>
 </table>
 <form id = "form" role = "form" class="form-inline" method="POST" action="address_book.php">
-	
+
 	<input id="name" name="name" >
 	<input id="phone" name="phone"  >
 	<input id="address" name="address" >
 	<input id="city" name="city" >
 	<input id="state" name="state" >
 	<input id="zip" name="zip" >
-	
+
 	<button class="btn">Add</button>
 </form>
 </body>
