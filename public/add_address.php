a1cd58bee91104eb7478d4fc4e04d6a11f9e8f01
denalie <denali.endicott@gmail.com>
2014-11-26 10:32:04 -0600

    Added to person class. Added id to url.

--- public/add_address.php
+++ public/add_address.php
@@ -1,23 +1,25 @@
 <?
 require '../inc/db_connect.php';
 
-
 class Person {
     public $id;
     public $first_name;
     public $last_name;
     public $phone;
 
-    public function add_address()
+    public function insert()
     {
-        return sprintf('<a href="/add_address.php/%s">%s</a>',$this->id);
-    }//end of function
+        $query = $this->dbc->prepare("INSERT INTO people(first_name, last_name, phone)
+                                      VALUES(:first_name, :last_name, :phone)");
 
+        $query->bindValue(':first_name', $this->first_name, PDO::PARAM_STR);
+        $query->bindValue(':last_name', $this->last_name, PDO::PARAM_STR);
+        $query->bindValue(':phone', $this->phone_number, PDO::PARAM_STR);
+
+        $query->execute();
+
+    }
 }//end of class
-
-    // $deleted_person = $dbc->prepare('DELETE FROM people WHERE id = :id');
-    // $deleted_person->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-    // $deleted_person->execute();
 
 if(isset($_GET['id'])) {
     $address_statment = $dbc->prepare('SELECT id, first_name, last_name, phone FROM people WHERE id =:id');
@@ -25,8 +27,9 @@ if(isset($_GET['id'])) {
     $address_statment->execute();
 
     $result = $address_statment->fetchObject("Person");
-    var_dump($result);
-}
+
+
+}//end if get set
 
 
 ?>
@@ -42,96 +45,85 @@ if(isset($_GET['id'])) {
 
     <link rel="stylesheet" type="text/css" href="css/addressbook_mysql.css">
 </head>
-<body class='container'>
+<body>
+<div class='container'>
+    <h1>Add Address to <?= $result->first_name ?> <?= $result->last_name ?></h1>
 
-    <h1>Add Address</h1>
+    <form role='form' method= "POST" action="add_address.php?id=<?=$_GET['id']?>">
+        <div class="form-group">
+            <label for='address'>Address:</label>
+            <input  type='text' class="form-control" id='address' name='address'>
+        </div>
 
-        <form role='form' method= "POST" action="add_address.php" id='border' class='img-rounded'>
-            <div class = 'row'>
-                <div class="form-group">
-                    <label class="col-md-5" for='address'>
-                        Address:
-                        <input  type='text' class="form-control" id='address' name='address'>
-                    </label>
-                </div>
-            </div>
+        <div class="form-group">
+            <label for='city'>City:</label>
+            <input  type='text' class="form-control" id='city' name='city'>
+        </div>
+        <div class="form-group">
+            <label for="state">State:</label>
+            <select id="state" name="state" class="form-control">
+                <option value="AL">AL</option>
+                <option value="AK">AK</option>
+                <option value="AZ">AZ</option>
+                <option value="AR">AR</option>
+                <option value="CA">CA</option>
+                <option value="CO">CO</option>
+                <option value="CT">CT</option>
+                <option value="DE">DE</option>
+                <option value="DC">DC</option>
+                <option value="FL">FL</option>
+                <option value="GA">GA</option>
+                <option value="HI">HI</option>
+                <option value="ID">ID</option>
+                <option value="IL">IL</option>
+                <option value="IN">IN</option>
+                <option value="IA">IA</option>
+                <option value="KS">KS</option>
+                <option value="KY">KY</option>
+                <option value="LA">LA</option>
+                <option value="ME">ME</option>
+                <option value="MD">MD</option>
+                <option value="MA">MA</option>
+                <option value="MI">MI</option>
+                <option value="MN">MN</option>
+                <option value="MS">MS</option>
+                <option value="MO">MO</option>
+                <option value="MT">MT</option>
+                <option value="NE">NE</option>
+                <option value="NV">NV</option>
+                <option value="NH">NH</option>
+                <option value="NJ">NJ</option>
+                <option value="NM">NM</option>
+                <option value="NY">NY</option>
+                <option value="NC">NC</option>
+                <option value="ND">ND</option>
+                <option value="OH">OH</option>
+                <option value="OK">OK</option>
+                <option value="OR">OR</option>
+                <option value="PA">PA</option>
+                <option value="RI">RI</option>
+                <option value="SC">SC</option>
+                <option value="SD">SD</option>
+                <option value="TN">TN</option>
+                <option value="TX">TX</option>
+                <option value="UT">UT</option>
+                <option value="VT">VT</option>
+                <option value="VA">VA</option>
+                <option value="WA">WA</option>
+                <option value="WV">WV</option>
+                <option value="WI">WI</option>
+                <option value="WY">WY</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for='zip'>Zip:</label>
+            <input type='number' class="form-control" id='zip' name='zip'>
+        </div>
 
-            <div class = 'row'>
-                <div class="form-group">
-                    <label class="col-md-3" for='city'>
-                        City:
-                        <input  type='text' class="form-control" id='city' name='city'>
-                    </label>
-                 <div class="col-lg-1 col-md-1 col-sm-1 col-xs-6">
-                    <label>
-                        State:
-                    </label>
-                        <br/>
-                            <select name="state" class="form-control">
-                                <option value="AL">AL</option>
-                                <option value="AK">AK</option>
-                                <option value="AZ">AZ</option>
-                                <option value="AR">AR</option>
-                                <option value="CA">CA</option>
-                                <option value="CO">CO</option>
-                                <option value="CT">CT</option>
-                                <option value="DE">DE</option>
-                                <option value="DC">DC</option>
-                                <option value="FL">FL</option>
-                                <option value="GA">GA</option>
-                                <option value="HI">HI</option>
-                                <option value="ID">ID</option>
-                                <option value="IL">IL</option>
-                                <option value="IN">IN</option>
-                                <option value="IA">IA</option>
-                                <option value="KS">KS</option>
-                                <option value="KY">KY</option>
-                                <option value="LA">LA</option>
-                                <option value="ME">ME</option>
-                                <option value="MD">MD</option>
-                                <option value="MA">MA</option>
-                                <option value="MI">MI</option>
-                                <option value="MN">MN</option>
-                                <option value="MS">MS</option>
-                                <option value="MO">MO</option>
-                                <option value="MT">MT</option>
-                                <option value="NE">NE</option>
-                                <option value="NV">NV</option>
-                                <option value="NH">NH</option>
-                                <option value="NJ">NJ</option>
-                                <option value="NM">NM</option>
-                                <option value="NY">NY</option>
-                                <option value="NC">NC</option>
-                                <option value="ND">ND</option>
-                                <option value="OH">OH</option>
-                                <option value="OK">OK</option>
-                                <option value="OR">OR</option>
-                                <option value="PA">PA</option>
-                                <option value="RI">RI</option>
-                                <option value="SC">SC</option>
-                                <option value="SD">SD</option>
-                                <option value="TN">TN</option>
-                                <option value="TX">TX</option>
-                                <option value="UT">UT</option>
-                                <option value="VT">VT</option>
-                                <option value="VA">VA</option>
-                                <option value="WA">WA</option>
-                                <option value="WV">WV</option>
-                                <option value="WI">WI</option>
-                                <option value="WY">WY</option>
-                            </select>
-                 </div>
-                    <label class="col-md-2" for='zip'>
-                        Zip:
-                        <input type='number' class="form-control" id='zip' name='zip' >
-                    </label>
-                </div>
-            </div>
-
-            <div class = 'row'>
-               <a href="?id=<?/* $result */ ?>" class="btn btn-default">Add Address</a>
-
-        </form>
-    </div>
+        <div class='form-group'>
+           <button type='submit' class="btn btn-default">Add</button>
+        </div>
+    </form>
+</div>
 </body>
 </html>
